f300ebd366a5e05baaefbd3829f512fb8facf036
b0mberb0b <ns_miticholas@yahoo.com>
2016-04-06 21:48:57 -0800

    followed loc8r layout, moved MEAN into own folder

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//require('./app_api/models/db');
 
-var routes = require('./NewCode/app_server/routes/index');
-var routesApi = require('./NewCode/app_api/routes/index');
-//var users = require('./app_server/routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -25,8 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/api', routesApi);
-//app.use('/users', users);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
